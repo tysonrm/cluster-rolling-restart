34633fef4188c1cab01fcaaf0f7e2724986fa2a2
tyson <tyson7757@comcast.net>
2021-03-30 07:31:28 -0500

    initial commit

---
+++ test/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send(`<h1>Hi from pid ${process.pid}</h1>`));
+
+app.get("/reload", (req, res) => {
+  res.send("performing rolling restart of cluster");
+  process.send({ cmd: "reload" });
+});
+
+function startServer(app) {
+  app.listen(8080, () => "server up");
+}
+
+require("../src/index").startCluster(startServer, app);
